1e0e9b951846129e97551b41757b7610053b9508
Ayushi Singla <ayushisingla46@gmail.com>
2021-11-24 14:30:38 +0530

    coupon microservice created

---
+++ couponservicedemo/src/main/java/com/ayushi/spring/couponservice/controller/CouponRestController.java
@@ -0,0 +1,30 @@
+package com.ayushi.spring.couponservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ayushi.spring.couponservice.model.Coupon;
+import com.ayushi.spring.couponservice.repos.CouponRepo;
+
+@RestController
+@RequestMapping("/couponapi")
+public class CouponRestController {
+
+	@Autowired
+	CouponRepo repo;
+
+	@RequestMapping(value = "/coupons", method = RequestMethod.POST)
+	public Coupon create(@RequestBody Coupon coupon) {
+		return repo.save(coupon);
+	}
+
+	@RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+	public Coupon getCoupon(@PathVariable("code") String code) {
+		System.out.println("server 2");
+		return repo.findByCode(code);
+	}
+}
